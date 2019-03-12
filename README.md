88466d8a8f0a3d02ed6c15a91ae93a5a1181c490
Spencer Christiansen <jc.spencer92@gmail.com>
2019-03-11 23:40:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Theme
+
+Create config files for my theme
+
+## Install
+
+1. Clone the repo `git clone git@github.com:spejamchr/theme.git`
+2. Install JS dependencies `yarn install`
+3. Install the image processor, [cairosvg][1]
+
+## Usage
+
+First, build the config files:
+
+```bash
+yarn biuld
+```
+
+Then symlink them into the right places.
+
+[1]: https://cairosvg.org/documentation/
---
+++ src/colors.js
@@ -0,0 +1,40 @@
+module.exports.colors = {
+
+  // A color set can have both light and dark variants, but is only required
+  // to have one.
+  dark: {
+
+    // Colors can be defined in any valid CSS format.
+
+    // accent0-7 should be the main accent colors of your theme. See the table
+    // in the "Color mappings" section for how the colors will be used in your
+    // new themes.
+    accent0: '#FF5555',
+    accent1: '#FFB86C',
+    accent2: '#F1FA8C',
+    accent3: '#50FA7B',
+    accent4: '#8BE9FD',
+    accent5: '#6272A4',
+    accent6: '#BD93F9',
+    accent7: '#FF79C6',
+
+    // shade0-7 should be shades of the same hue, with shade0 being the
+    // background and shade7 being the foreground. If you omit the
+    // intermediate shades (1 through 6), they will be calculated automatically
+    // for you.
+    shade0: '#282A36',
+    shade1: '#44475A',
+    shade2: '#63727D',
+    shade3: '#627876',
+    shade4: '#81968B',
+    shade5: '#A6B8A5',
+    shade6: '#D2D9CC',
+    shade7: '#F8F8F2'
+
+  },
+
+  // Same as above, except that shade0 should be the lightest and shade7 should
+  // be the darkest.
+  // light: { ... },
+
+};
